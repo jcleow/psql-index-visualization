6ea7643fc127e1deb9de7bf1b1cd61432fad0cde
jcleow <>
2023-03-23 15:39:13 +0800

    minor changes

--- src/index.js
+++ src/index.js
@@ -37,36 +37,3 @@ const createChart = (label) => {
     createChart(label);
   });
 })();
-
-// \copy (SELECT row_to_json(q) FROM
-//     (
-//         SELECT
-//             idstat.relname AS TABLE_NAME,
-//             indexrelname AS index_name,
-//             idstat.idx_scan AS index_scans_count,
-//             idstat.idx_tup_read as index_scan_tuple_read,
-//             idstat.idx_tup_fetch as index_scan_tuple_fetch,
-//             pg_relation_size(indexrelid) AS index_size,
-//             tabstat.idx_scan AS table_reads_index_count,
-//             tabstat.seq_scan AS table_reads_seq_count,
-//             tabstat.seq_scan + tabstat.idx_scan AS table_reads_count,
-//             n_tup_upd + n_tup_ins + n_tup_del AS table_writes_count,
-//             pg_relation_size(idstat.relid) AS table_size
-//         FROM
-//             pg_stat_user_indexes AS idstat
-//         JOIN
-//             pg_indexes
-//             ON
-//             indexrelname = indexname
-//             AND
-//             idstat.schemaname = pg_indexes.schemaname
-//         JOIN
-//             pg_stat_user_tables AS tabstat
-//             ON
-//             idstat.relid = tabstat.relid
-//         WHERE
-//             indexdef !~* 'unique'
-//         ORDER BY
-//             idstat.idx_scan DESC,
-//             pg_relation_size(indexrelid) DESC
-//     ) q) to '/Users/jitcorn/index.json'
